fd871b872cd0c101451504ff94d1daf0dcbc55c5
Chuck Krutsinger <chuck.krutsinger@ksoftllc.com>
2024-02-13 20:22:11 -0700

    swipes not reliably firing

---
+++ src/SwipeGesture/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SwipeGesture
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/SwipeGesture/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace SwipeGesture
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public List<string> Items {  get; set; } = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliet", "kilo", "lima", "mike", "november"];
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+        {
+            Debug.Write($"swiped {e.Direction}");
+        }
+
+        private void RefreshView_Refreshing(object sender, EventArgs e)
+        {
+            //refreshView.IsRefreshing = false;
+        }
+    }
+
+}
